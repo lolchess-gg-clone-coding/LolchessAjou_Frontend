baddf3cfce8a1702a1ac564e9a0ff8fa27c46c27
knamuuu <knamuuu@naver.com>
2022-05-16 13:17:21 +0900

    Modify ProfilePage: 코드 간소화

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -4,41 +4,27 @@ import { useLocation } from "react-router-dom";
 function ProfilePage() {
   const { state } = useLocation();
 
-  const user = state[0];
-  const rank_tier = state[1];
-  const superfast_tier = state[2];
-
-  const [Nickname] = useState(user.nickname);
-  const [Levels] = useState(user.levels);
-  const [IconId] = useState(user.icon_id);
-  const [Tier] = useState(rank_tier.tier);
-  const [SubTier] = useState(rank_tier.sub_tier);
-  const [LeaguePoint] = useState(rank_tier.league_point);
-  const [Date] = useState(rank_tier.date);
-  const [Win] = useState(rank_tier.win);
-  const [Defeat] = useState(rank_tier.defeat);
-  const [Top4] = useState(rank_tier.top4);
-  const [SFTier] = useState(superfast_tier.sf_tier);
-  const [SFLeaguePoint] = useState(superfast_tier.sf_league_point);
-  const [SFDate] = useState(superfast_tier.sf_date);
+  const [UserInfo] = useState(state[0]);
+  const [RankInfo] = useState(state[1]);
+  const [SFInfo] = useState(state[2]);
 
   return (
     <div>
-      <div>nickname: {Nickname}</div>
-      <div>levels: {Levels}</div>
-      <div>icon_id: {IconId}</div>
+      <div>nickname: {UserInfo.nickname}</div>
+      <div>levels: {UserInfo.levels}</div>
+      <div>icon_id: {UserInfo.icon_id}</div>
       <hr />
-      <div>tier: {Tier}</div>
-      <div>sub_tier: {SubTier}</div>
-      <div>league_point: {LeaguePoint}</div>
-      <div>date: {Date}</div>
-      <div>win: {Win}</div>
-      <div>defeat: {Defeat}</div>
-      <div>top4: {Top4}</div>
+      <div>tier: {RankInfo.tier}</div>
+      <div>sub_tier: {RankInfo.sub_tier}</div>
+      <div>league_point: {RankInfo.league_point}</div>
+      <div>date: {RankInfo.date}</div>
+      <div>win: {RankInfo.win}</div>
+      <div>defeat: {RankInfo.defeat}</div>
+      <div>top4: {RankInfo.top4}</div>
       <hr />
-      <div>sf_tier: {SFTier}</div>
-      <div>sf_league_point: {SFLeaguePoint}</div>
-      <div>sf_date_in_tier: {SFDate}</div>
+      <div>sf_tier: {SFInfo.sf_tier}</div>
+      <div>sf_league_point: {SFInfo.sf_league_point}</div>
+      <div>sf_date_in_tier: {SFInfo.sf_date}</div>
     </div>
   );
 }
