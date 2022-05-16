c2b0e613f20e715828613f73b86fe64b7430da8e
knamuuu <knamuuu@naver.com>
2022-05-17 00:27:05 +0900

    Modify ProfilePage.js: userinfo 배경추가 및 아이콘 div 설정/승률, top4비율 추가

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,44 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+import summonerImg from "../images/bg-summoner.jpeg";
+
+const UserInfoBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 9fr;
+  padding: 20px;
+
+  font-weight: 700;
+
+  background-image: url(${summonerImg});
+  background-size: cover;
+`;
+
+const IconBox = styled.div`
+  position: relative;
+
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+
+  color: white;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+`;
+
+const RankBox = styled.div`
+  border: 1px solid black;
+`;
+
+const RankDetail = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+`;
 
 function ProfilePage() {
   const { state } = useLocation();
@@ -8,23 +47,85 @@ function ProfilePage() {
   const [RankInfo] = useState(state[1]);
   const [SFInfo] = useState(state[2]);
 
+  function Sumgame(win, defeat) {
+    let allGame = win + defeat;
+
+    return allGame;
+  }
+
+  function Rate(num, allGame) {
+    let rate = (num / allGame) * 100;
+
+    return rate.toFixed(1);
+  }
+
   return (
-    <div>
-      <div>nickname: {UserInfo.nickname}</div>
-      <div>levels: {UserInfo.levels}</div>
-      <div>icon_id: {UserInfo.icon_id}</div>
+    <div style={{ padding: "20px 200px" }}>
+      <UserInfoBox>
+        <IconBox>
+          <div>icon_id: {UserInfo.icon_id}</div>
+          <div
+            style={{
+              bottom: "0",
+              right: "0",
+              position: "absolute",
+              padding: "4px 6px",
+              backgroundColor: "#fff",
+              color: "black",
+              fontWeight: "700",
+              fontSize: "12px",
+            }}
+          >
+            levels: {UserInfo.levels}
+          </div>
+        </IconBox>
+        <div>
+          <span style={{ fontSize: "24px", color: "white" }}>
+            nickname: {UserInfo.nickname}
+          </span>
+        </div>
+      </UserInfoBox>
       <hr />
-      <div>tier: {RankInfo.tier}</div>
-      <div>sub_tier: {RankInfo.sub_tier}</div>
-      <div>league_point: {RankInfo.league_point}</div>
-      <div>date: {RankInfo.date}</div>
-      <div>win: {RankInfo.win}</div>
-      <div>defeat: {RankInfo.defeat}</div>
-      <div>top4: {RankInfo.top4}</div>
-      <hr />
-      <div>sf_tier: {SFInfo.sf_tier}</div>
-      <div>sf_league_point: {SFInfo.sf_league_point}</div>
-      <div>sf_date_in_tier: {SFInfo.sf_date}</div>
+      <Container>
+        <RankBox>
+          <div className="rankInfo">
+            <div>
+              tier: {RankInfo.tier} {RankInfo.sub_tier}
+            </div>
+            <div>league_point: {RankInfo.league_point} LP</div>
+          </div>
+          <RankDetail>
+            <div>
+              <span>승리 </span>
+              <span>{RankInfo.win}</span>
+            </div>
+            <div>
+              <span>승률 </span>
+              <span>
+                {Rate(RankInfo.win, Sumgame(RankInfo.win, RankInfo.defeat))}%
+              </span>
+            </div>
+            <div>top4 {RankInfo.top4}</div>
+            <div>
+              <span>top4 비율 </span>
+              <span>
+                {Rate(RankInfo.top4, Sumgame(RankInfo.win, RankInfo.defeat))}%
+              </span>
+            </div>
+            <div>
+              <span>게임수 </span>
+              <span>{Sumgame(RankInfo.win, RankInfo.defeat)}</span>
+            </div>
+            <div>평균 등수</div>
+          </RankDetail>
+        </RankBox>
+        <div className="lpChart" style={{ border: "1px solid black" }}></div>
+        <div className="sfBox">
+          <div>
+            sf_tier: {SFInfo.sf_tier} {SFInfo.sf_league_point}점
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
